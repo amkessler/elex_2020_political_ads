c25e5f9a4a1ee1f2f871b158b52aeaf0ba3457c7
Kessler <ak113714@bna.com>
2020-11-15 13:40:51 -0500

    initial commit

---
+++ 00_b_load_data_pullfrom_db_POSTELECTION.R
@@ -0,0 +1,88 @@
+library(tidyverse)
+library(DBI)
+library(RMariaDB)
+
+##add user name and pw in your .Renvrion file
+con <- dbConnect(MariaDB(), 
+                 user=Sys.getenv("ADANALYTICS_USER"), 
+                 password=Sys.getenv("ADANALYTICS_PW"), 
+                 host="products.r53.advertisinganalyticsllc.com",
+                 port="3306", 
+                 dbname="products")
+
+#list tables in database
+dbplyr::src_dbi(con)
+
+#pull main table into db object
+admo_main_db <- tbl(con, "vw_bloomberg_political_airings")
+
+glimpse(admo_main_db)
+
+#set time for timestamp columns below
+current_time <- Sys.time()
+
+### PRESIDENTIAL ####
+
+#pulling down presidential ad data into dataframe - from March 1 onwards
+prez_ads_all <- admo_main_db %>% 
+  filter(str_detect(election, "Presidential 2020"),
+         airdate >= "2020-11-04") %>% 
+  collect()
+
+#add timestamp
+prez_ads_all$bb_data_date <- current_time
+
+#save as rds
+saveRDS(prez_ads_all, "raw_data/prez_ads_all_postelex.rds")
+
+
+#### HOUSE AND SENATE ####
+
+# pulling down senate races
+senate_ads_all <- admo_main_db %>% 
+  filter(str_detect(election, "Senate"),
+         airdate >= "2020-11-04") %>% 
+  collect()
+
+#add timestamp
+senate_ads_all$bb_data_date <- current_time
+
+
+# pulling down house races
+house_ads_all <- admo_main_db %>% 
+  filter(str_detect(election, "CD-"),
+         airdate >= "2020-11-04") %>% 
+  collect()
+
+#add timestamp
+house_ads_all$bb_data_date <- current_time
+
+
+#save as rds
+saveRDS(senate_ads_all, "raw_data/senate_ads_all_postelex.rds")
+#save as rds
+saveRDS(house_ads_all, "raw_data/house_ads_all_postelex.rds")
+
+
+
+
+#### DELTA DATABASE ####
+
+#pull main table into db object
+
+# admo_delta_db <- tbl(con, "vw_bloomberg_delta_spending")
+# 
+# glimpse(admo_delta_db)
+# 
+# temp_delta <- admo_delta_db %>% 
+#   head() %>% 
+#   collect()
+# 
+# temp_delta
+
+  
+
+#disconnect
+dbDisconnect(con)
+
+
---
+++ 02_ga_workingfile.R
@@ -0,0 +1,76 @@
+library(tidyverse)
+library(lubridate)
+library(janitor)
+library(gt)
+library(kableExtra)
+options(dplyr.summarise.inform = FALSE)
+
+### LOAD AND PROCESSING #### 
+
+#load saved files from db pulls done in step 00_B
+senate_ads_all <- readRDS("raw_data/senate_ads_all_postelex.rds")
+house_ads_all <- readRDS("raw_data/house_ads_all_postelex.rds")
+prez_ads_all <- readRDS("raw_data/prez_ads_all_postelex.rds")
+
+#add field to mark senate/house
+senate_ads_all <- senate_ads_all %>% 
+  mutate(
+    office = "senate"
+  )
+
+house_ads_all <- house_ads_all %>% 
+  mutate(
+    office = "house"
+  )
+
+prez_ads_all <- prez_ads_all %>% 
+  mutate(
+    office = "prez"
+  )
+
+
+# combine into one (can also work with each separately if desired)
+admo_main <- bind_rows(prez_ads_all, senate_ads_all, house_ads_all)
+
+#add date-derived fields
+admo_main <- admo_main %>% 
+  mutate(
+    airdate_year = year(airdate),
+    airdate_month = month(airdate),
+    airdate_isoweek = isoweek(airdate),
+    airdate_day = day(airdate)
+  )
+
+#pull out GA runoff records only, tv ads
+ga_runoff_tvads <- admo_main %>% 
+  filter(election_state == "GA",
+         election %in% c("GA Senate 2020 General Runoff", "GA Senate Special 2020 General Runoff"),
+         !station %in% c("Facebook", "Google")) #take out social media ads so it's tv airings only
+
+
+
+
+### ANALYSIS ####
+
+ga_runoff_tvads %>% 
+  group_by(election, advertiser_party, advertiser) %>% 
+  summarise(ads_aired = n(), est_spent = sum(spent)) 
+
+
+ga_runoff_tvads %>% 
+  count(advertiser_party, tone, name = "ads_aired") 
+
+
+ga_runoff_tvads %>% 
+  group_by(advertiser_party, market) %>% 
+  summarise(ads_aired = n())
+
+
+ga_runoff_tvads %>% 
+  count(advertiser_party, market) %>% 
+  slice_max(order_by = n, n = 5)
+
+
+ga_runoff_tvads %>% 
+  count(advertiser_party, market) %>% 
+  top_n(n = 5, wt = market)
